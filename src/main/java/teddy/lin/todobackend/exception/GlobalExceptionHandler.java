b025d6d41f3c31cf6abba4cbc39544dacfc211da
theroadof <1294911641@qq.com>
2020-08-07 13:08:22 +0800

    [todo-backend][Teddy] feature:implement exception handle

--- src/main/java/teddy/lin/todobackend/exception/GlobalExceptionHandler.java
+++ src/main/java/teddy/lin/todobackend/exception/GlobalExceptionHandler.java
@@ -1,14 +1,14 @@
 package teddy.lin.todobackend.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import teddy.lin.todobackend.exception.exceptions.IllegalUpdateIDException;
-import teddy.lin.todobackend.exception.messages.ExceptionMessage;
 import teddy.lin.todobackend.exception.exceptions.NoSuchTodoException;
+import teddy.lin.todobackend.exception.messages.ExceptionMessage;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchTodoException.class)
